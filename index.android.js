0d3d916055e07adc0ae6bfe76e303b6621ae8462
hitchs3x <hitchs3x@gmail.com>
2017-10-26 14:50:33 +0300

    import Exchange component as root for testing

--- index.android.js
+++ index.android.js
@@ -1,3 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-AppRegistry.registerComponent('cryptotrader', () => App);
+import Exchange from './android/app/src/components/ExchangeScreen'
+AppRegistry.registerComponent('cryptotrader', () => Exchange);
